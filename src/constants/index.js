2bcef69c882ac3f7459a0ab24958d5431dd57262
harikrishnan-git-2000 <harikrishnan.2000@outlook.com>
2025-04-28 00:09:20 -0700

    hero svg

--- src/constants/index.js
+++ src/constants/index.js
@@ -16,9 +16,9 @@ const navLinks = [
 
 const words = [
     { text: "Ideas", imgPath: "./images/ideas.svg" },
-    { text: "Concepts", imgPath: "./images/concepts.svg" },
-    { text: "Designs", imgPath: "./images/designs.svg" },
-    { text: "Code", imgPath: "./images/code.svg" },
+    { text: "Concepts", imgPath: "./images/ideas.svg" },
+    { text: "Designs", imgPath: "./images/ideas.svg" },
+    { text: "Code", imgPath: "./images/ideas.svg" },
 
 ];
 
