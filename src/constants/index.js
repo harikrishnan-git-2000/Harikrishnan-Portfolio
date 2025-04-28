bdb66b8b79a61cb59c6c84ac68d3d3bbad7be5e0
harikrishnan-git-2000 <harikrishnan.2000@outlook.com>
2025-04-27 23:37:18 -0700

    hero words svg

--- src/constants/index.js
+++ src/constants/index.js
@@ -15,14 +15,14 @@ const navLinks = [
 ];
 
 const words = [
-    { text: "Ideas", imgPath: "public/images/ideas.svg" },
-    { text: "Concepts", imgPath: "public/images/ideas.svg" },
-    { text: "Designs", imgPath: "public/images/ideas.svg" },
-    { text: "Code", imgPath: "public/images/code.svg" },
-    { text: "Ideas", imgPath: "public/images/ideas.svg" },
-    { text: "Concepts", imgPath: "public/images/ideas.svg" },
-    { text: "Designs", imgPath: "public/images/ideas.svg" },
-    { text: "Code", imgPath: "public/images/code.svg" },
+    { text: "Ideas", imgPath: "/images/ideas.svg" },
+    { text: "Concepts", imgPath: "/images/concepts.svg" },
+    { text: "Designs", imgPath: "/images/designs.svg" },
+    { text: "Code", imgPath: "/images/code.svg" },
+    { text: "Ideas", imgPath: "/images/ideas.svg" },
+    { text: "Concepts", imgPath: "/images/concepts.svg" },
+    { text: "Designs", imgPath: "/images/designs.svg" },
+    { text: "Code", imgPath: "/images/code.svg" },
 ];
 
 const techStackIcons = [
