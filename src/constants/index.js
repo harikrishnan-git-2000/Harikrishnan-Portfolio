8cfb77814b581ee5f4bf57d131bb5dee8ba2fd33
harikrishnan-git-2000 <harikrishnan.2000@outlook.com>
2025-04-27 23:47:09 -0700

    we'll see

--- src/constants/index.js
+++ src/constants/index.js
@@ -16,7 +16,7 @@ const navLinks = [
 
 const words = [
     { text: "Ideas", imgPath: "./images/ideas.svg" },
-    { text: "Concepts", imgPath: "/images/concepts.svg" },
+    { text: "Concepts", imgPath: "./images/concepts.svg" },
     { text: "Designs", imgPath: "./images/designs.svg" },
     { text: "Code", imgPath: "./images/code.svg" },
 
